970a63271412f5370e7bee30e0b9f4c1e02cf7d6
machdragon <machdragon963852741@gmail.com>
2022-02-22 22:17:39 -0800

    Added Appointment, Payable and Tests
    Added Appointment, Payable to Solidity Smart Contract to pay ETH
    and Tests to verify code

--- contracts/Calend3eth.sol
+++ contracts/Calend3eth.sol
@@ -3,10 +3,20 @@ pragma solidity ^0.8.0;
 
 contract Calend3eth {
     uint rate;
-    address owner;
+    address payable owner;
+
+    struct Appointment {
+    string title;     // title of the meeting
+    address attendee; // person you are meeting
+    uint startTime;   // start time of meeting
+    uint endTime;     // end time of the meeting
+    uint amountPaid;  // amount paid for the meeting
+    }
+
+    Appointment[] appointments;
 
     constructor() {
-        owner = msg.sender;
+        owner = payable(msg.sender);
     }
 
     function getRate() public view returns (uint) {
@@ -16,5 +26,27 @@ contract Calend3eth {
     function setRate(uint _rate) public {
         require(msg.sender == owner, "Only the owner can set the rate");
         rate = _rate;
+    }
+
+    // when returning string needs to allocate temporary place to store data
+    // we do this with the memory keyword in Solidity
+    function getAppointments() public view returns (Appointment[] memory) {
+        return appointments;
+    }
+
+    function createAppointment(string memory title, uint startTime, uint endTime) public payable {
+        Appointment memory appointment;
+        appointment.title = title;
+        appointment.startTime = startTime;
+        appointment.endTime = endTime;
+        appointment.amountPaid = ((endTime - startTime) / 60) * rate;
+        appointment.attendee = msg.sender; // address of person calling contract
+
+        require(msg.value >= appointment.amountPaid, "We require more ether"); // validate the amount of ETH
+
+        (bool success,) = owner.call{value: msg.value}(""); // send ETH to the owner
+        require(success, "Failed to send Ether");
+
+        appointments.push(appointment);
     }
 }
--- test/test.js
+++ test/test.js
@@ -2,11 +2,21 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Calend3eth", function () {
-  it("Should set the minutely rate", async function () {
-    const Contract = await ethers.getContractFactory("Calend3eth");
-    const contract = await Contract.deploy();
-    await contract.deployed();
+  let Contract, contract;
+  let owner, addr1, addr2;
 
+  // 'beforeEach' will run before each test 
+  // define this common global code first to be used later 
+  // refactor code to split it below into 2 tests functions
+  beforeEach(async function () {
+    [owner, addr1, addr2] = await ethers.getSigners();
+
+    Contract = await ethers.getContractFactory("Calend3eth");
+    contract = await Contract.deploy();
+    await contract.deployed();
+  });
+
+  it("Should set the minutely rate", async function () {
     const tx = await contract.setRate(1000);
 
     // wait until the transaction is mined
@@ -14,17 +24,36 @@ describe("Calend3eth", function () {
 
     // verify rate is set correctly
     expect(await contract.getRate()).to.equal(1000);
+  });
 
-    // get addresses
-    const [owner, addr1, addr2] = await ethers.getSigners();
-
-    // console.log(owner.address)
-    // console.log(addr1.address)
-
+  it("Should fail if non-owner rate", async function () {
     // call setRate using a different account address
     // this should fail since this address is not the owner
     await expect(
-      contract.connect(addr1).setRate(500)
-    ).to.be.revertedWith('Only the owner can set the rate')
+      contract.connect(addr1).setRate(500))
+      .to.be.revertedWith('Only the owner can set the rate');
+  });
+
+  it("Should add two appointments", async function () {
+    const tx1 = await contract.setRate(ethers.utils.parseEther("0.001"));
+    await tx1.wait();
+  
+    const tx2 = await contract.connect(addr1).createAppointment("Meeting with Part Time Larry", 1644143400, 1644150600, {value: ethers.utils.parseEther("2")});
+    await tx2.wait();
+
+    const tx3 = await contract.connect(addr2).createAppointment("Breakfast at Tiffany's", 1644154200, 1644159600, {value: ethers.utils.parseEther("1.5")});
+    await tx3.wait();
+
+    const appointments = await contract.getAppointments();
+
+    expect(appointments.length).to.equal(2);
+
+    const ownerBalance = await ethers.provider.getBalance(owner.address);
+    const addr1Balance = await ethers.provider.getBalance(addr1.address);
+    const addr2Balance = await ethers.provider.getBalance(addr2.address);
+
+    console.log(ownerBalance);
+    console.log(addr1Balance);
+    console.log(addr2Balance);
   });
 });
