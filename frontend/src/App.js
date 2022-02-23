965c1e640be112ebb05e86de65021f17f6b31697
machdragon <machdragon963852741@gmail.com>
2022-02-22 20:36:11 -0800

    Commit React Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,61 @@
+import './App.css';
+import detectEthereumProvider from '@metamask/detect-provider';
+import {useState, useEffect} from 'react';
+import Calendar from './components/Calendar';
+
+function App() {
+
+  // makes a pop up with connect as text
+  // const connect = () => {
+  //   alert('connect');
+  // }
+
+  const [account, setAccount] = useState(false);
+
+  useEffect(() => {
+    isConnected();
+  }, []);
+
+  const isConnected = async () => {
+    const provider = await detectEthereumProvider();
+    const accounts = await provider.request({ method: "eth_accounts" });
+
+    if (accounts.length > 0) {
+      setAccount(accounts[0]);
+    } else {
+      console.log("No authorized account found")
+    }
+  }
+
+  const connect = async () => {
+    try {
+      const provider = await detectEthereumProvider();
+      
+      // returns an array of accounts
+      const accounts = await provider.request({ method: "eth_requestAccounts" });
+      
+      // check if array at least one element
+      if (accounts.length > 0) {
+        console.log('account found', accounts);
+        setAccount(accounts[0]);
+      } else {
+        console.log('No account found');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>calend3.eth</h1>
+        <div id = "slogan"> web3 appointment scheduling</div>
+        {!account && <button onClick={connect}>connect wallet</button>}
+        {account && <Calendar/>}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Calendar.js
@@ -0,0 +1,5 @@
+const Calendar = () => {
+    return <div id="calendar">This is the calendar component</div>;
+}
+
+export default Calendar;
