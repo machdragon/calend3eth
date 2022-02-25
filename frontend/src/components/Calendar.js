e1ad72deaf92e8ce1fa83323a9f2c70299696a0c
machdragon <machdragon963852741@gmail.com>
2022-02-24 17:03:42 -0800

    Added Calendar UI

--- frontend/src/components/Calendar.js
+++ frontend/src/components/Calendar.js
@@ -1,5 +1,28 @@
+/* Scheduling Component */
+import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
+import { Scheduler, WeekView, Appointments, AppointmentForm } from '@devexpress/dx-react-scheduler-material-ui';
+
+const schedulerData = [
+    { startDate: '2022-02-24T09:45', endDate: '2022-02-24T11:00', title: 'Dogecoin Integration' },
+    { startDate: '2022-02-25T12:00', endDate: '2022-02-25T13:30', title: 'Podcast Appearance' },
+  ];
+
+const saveAppointment = (data) => {
+    console.log('appointment saved');
+    console.log(data);
+}
+
 const Calendar = () => {
-    return <div id="calendar">This is the calendar component</div>;
+    return <div id="calendar">
+        <Scheduler data={schedulerData}>
+            <ViewState />
+            <EditingState onCommitChanges={saveAppointment} />
+            <IntegratedEditing />
+            <WeekView startDayHour={9} endDayHour={19}/>
+            <Appointments />
+            <AppointmentForm />
+        </Scheduler>
+    </div>;
 }
 
 export default Calendar;
